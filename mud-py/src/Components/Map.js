70222e9ab3f5e68990584f6f284af8bbf12368bf
Colin Dismuke <colin.dismuke@gmail.com>
2019-07-13 21:47:47 -0500

    Added MarkSeries to show rooms on map

--- mud-py/src/Components/Map.js
+++ mud-py/src/Components/Map.js
@@ -3,7 +3,26 @@ import { FlexibleXYPlot, LineSeries, MarkSeries } from "react-vis";
 import styled from "styled-components";
 
 export default class Map extends Component {
+  state = { value: null };
+
   render() {
-    return <div />;
+    const { value } = this.state;
+    const { coordinates, graph } = this.props;
+
+    return (
+      <FlexibleXYPlot>
+        <MarkSeries
+          strokeWidth={3}
+          opacity="1"
+          size="5"
+          color="FF0"
+          data={coordinates}
+          style={{ cursor: "pointer" }}
+          onValueClick={point => {}}
+          onValueMouseOver={point => {}}
+          onValueMouseOut={() => {}}
+        />
+      </FlexibleXYPlot>
+    );
   }
 }
