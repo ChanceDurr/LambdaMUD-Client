00a482c392d40259eb5d65767dda344aadbb16ae
Colin Dismuke <colin.dismuke@gmail.com>
2019-07-16 22:08:51 -0500

    Added react-vis to the correct package.json

--- mud-py/src/Components/Landing.js
+++ mud-py/src/Components/Landing.js
@@ -1,15 +1,6 @@
 import React, { Component } from "react";
 import pyman_logo from "../assets/pyman_logo@2x.png";
-import styled from "styled-components";
 import "./landing.css";
-
-const LandingContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  margin: 0 auto;
-  padding-top: 100px;
-  max-width: 100%;
-`;
 
 export default class Landing extends Component {
   render() {
