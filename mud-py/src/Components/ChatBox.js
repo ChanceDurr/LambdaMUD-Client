18bcbba53bcb8e7fca4a6c24871b548368b44e07
Kyle Baker <tokylebaker@gmail.com>
2019-07-19 19:14:03 -0700

    realtime chat

--- mud-py/src/Components/ChatBox.js
+++ mud-py/src/Components/ChatBox.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const FlexColumn = styled.div`
   display: flex;
   flex-direction: column;
-  height: ${props => props.height || 'auto'};
-  flex-grow: ${props => props.grow || '0'}
-`
+  height: ${props => props.height || "auto"};
+  flex-grow: ${props => props.grow || "0"};
+`;
 
 const FlexRow = styled.div`
   display: flex;
   flex-direction: row;
-  width: ${props => props.width || 'auto'};
+  width: ${props => props.width || "auto"};
   justify-content: ${props =>
-  props.justifyEnd
+    props.justifyEnd
       ? `flex-end`
       : props.justifyBetween
       ? `space-between`
@@ -23,11 +23,10 @@ const FlexRow = styled.div`
       ? `center`
       : `flex-start`};
   align-items: ${props =>
-      props.alignEnd ? `flex-end` : props.alignCenter ? `center` : `flex-start`};
-   
-   margin-top: 5px;
+    props.alignEnd ? `flex-end` : props.alignCenter ? `center` : `flex-start`};
 
-`
+  margin-top: 5px;
+`;
 
 const TextArea = styled.textarea`
   flex-grow: 1;
@@ -46,25 +45,37 @@ const Send = styled.button`
   background: lawngreen;
   color: #2e5d01;
   font-weight: bold;
-`
+`;
 
-function ChatBox ({ incomingMessage, onSpeak, handleMessageInput, message }) {
-  return <FlexColumn height="100%">
-    <Feed grow="1">
-      <span>Welcome to MudPy! This is the beginning of your chat. As you move through the rooms you can speak to other players in the same room as you.  Good muck!</span>
-    </Feed>
+function ChatBox({ messageFeed, onSpeak, handleMessageInput, message }) {
+  return (
+    <FlexColumn height="100%">
+      <Feed grow="1">
+        <span style={{ marginBottom: "10px" }}>
+          Welcome to MudPy! This is the beginning of your chat. As you move
+          through the rooms you can speak to other players in the same room as
+          you. Good muck and glob speed!
+        </span>
+        {messageFeed.map(feed => (
+          <span>
+            {feed.player}: {feed.message}
+          </span>
+        ))}
+      </Feed>
 
-    <FlexRow width="100%" alignCenter>
-      <TextArea
-        value={message}
-        onChange={e => handleMessageInput(e)}
-      />
+      <FlexRow width="100%" alignCenter>
+        <TextArea
+          value={message}
+          onChange={handleMessageInput}
+          onKeyUp={handleMessageInput}
+        />
 
-      <Send onClick={onSpeak}>SEND</Send>
-    </FlexRow>
-
-  </FlexColumn>
+        <Send type="submit" onClick={onSpeak}>
+          SEND
+        </Send>
+      </FlexRow>
+    </FlexColumn>
+  );
 }
 
-export default ChatBox
-
+export default ChatBox;
