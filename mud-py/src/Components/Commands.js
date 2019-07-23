b5c7d079706b4357a6685cb4c997ca19b1bf11f9
Kyle Baker <tokylebaker@gmail.com>
2019-07-22 20:04:49 -0700

    scroll when user2 speaks

--- mud-py/src/Components/Commands.js
+++ mud-py/src/Components/Commands.js
@@ -19,42 +19,66 @@ function Commands(props) {
   return (
     <Box>
       <div>Please choose a direction.</div>
-    <Box
-      width="100%"
-      height="10vh"
-      border={2}
-      borderColor="#7b68ee"
-      display="flex"
-      flexDirection="row"
-      justifyContent="space-between"
-      alignItems="center"
-      p="10px"
-    >
-      <div style={{display:"flex", flexDirection:"column", alignItems: "center"}} >
-        <div style={{margin: "auto"}}>North</div>
-      <CommandButton onClick={val => props.directionMove(val)} name="n">
-        N
-      </CommandButton>
-      </div>
-      <div style={{display:"flex", flexDirection:"column", alignItems: "center"}} >
-        <div style={{margin: "auto"}}>South</div>
-      <CommandButton onClick={props.directionMove} name="s">
-        S
-      </CommandButton>
-      </div>
-      <div style={{display:"flex", flexDirection:"column", alignItems: "center"}} >
-        <div style={{margin: "auto"}}>East</div>
-      <CommandButton onClick={props.directionMove} name="e">
-        E
-      </CommandButton>
-      </div>
-      <div style={{display:"flex", flexDirection:"column", alignItems: "center"}} >
-        <div style={{margin: "auto"}}>West</div>
-      <CommandButton onClick={props.directionMove} name="w">
-        W
-      </CommandButton>
-      </div>
-    </Box>
+      <Box
+        width="100%"
+        height="10vh"
+        border={2}
+        borderColor="#7b68ee"
+        display="flex"
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center"
+        p="10px"
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center"
+          }}
+        >
+          <div style={{ margin: "auto" }}>North</div>
+          <CommandButton onClick={val => props.directionMove(val)} name="n">
+            N
+          </CommandButton>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center"
+          }}
+        >
+          <div style={{ margin: "auto" }}>South</div>
+          <CommandButton onClick={props.directionMove} name="s">
+            S
+          </CommandButton>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center"
+          }}
+        >
+          <div style={{ margin: "auto" }}>East</div>
+          <CommandButton onClick={props.directionMove} name="e">
+            E
+          </CommandButton>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center"
+          }}
+        >
+          <div style={{ margin: "auto" }}>West</div>
+          <CommandButton onClick={props.directionMove} name="w">
+            W
+          </CommandButton>
+        </div>
+      </Box>
       {/* this ternary displays a directional error message  */}
       {props.error_msg === "" ? null : <div> {props.error_msg} </div>}
     </Box>
